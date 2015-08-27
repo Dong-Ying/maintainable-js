5a57f41faec61b308987eacf8ea814875b9c11a7
mengru zhao <mrzhao@thoughtworks.com>
2015-08-27 08:33:45 +0800

    refactor the toggle of like

--- src/js/app.js
+++ src/js/app.js
@@ -3,12 +3,8 @@ $(function() {
         {'id':0, "name": "Melbourne", "description": "A great place", status: true}
     ];
 
-    var likedPlaces = [
-        {"id":0,"name": "Melbourne"}
-    ];
-
     var searchResultsModel = new SearchResults({'searchResults': searchResults});
-    var likedPlacesModel = new LikedPlaces({'likedPlaces': likedPlaces});
+    var likedPlacesModel = new LikedPlaces({'likedPlaces': []});
 
     var searchFormView = new SearchFormView(searchResultsModel);
     var searchResultsView = new SearchResultsView(searchResultsModel, likedPlacesModel);
--- src/js/search-results-view.js
+++ src/js/search-results-view.js
@@ -11,29 +11,24 @@ var SearchResultsView = Backbone.View.extend({
     },
 
     likeLocation: function (e) {
-
         e.preventDefault();
         //toggle search result status
         var id = $(e.currentTarget).data('id');
-        console.log(id);
         var searchResults = this.searchResultsmodel.get('searchResults');
         var current = _.findWhere(searchResults, {"id": id});
-        current.status = !current.status;
-        this.searchResultsmodel.trigger('change:searchResults', searchResults);
         //trigger liked places
         var likedPlaces = this.likedPlacesModel.get('likedPlaces');
         if(current.status){
-            console.log("current status is true");
             likedPlaces.push({id: id, name: $($(e.currentTarget)[0]).parent().find('h5').text()});
         }else{
-            console.log("current status is false");
             _.each(likedPlaces,function(value,index){
-                console.log(value.id,value.name, '++++++++++++++++++++++++++++');
             })
             this.likedPlacesModel.set('likedPlaces',_.filter(likedPlaces,function(element){
                 return element.id !=id
             }))
         }
+        current.status = !current.status;
+        this.searchResultsmodel.trigger('change:searchResults', searchResults);
         this.likedPlacesModel.trigger('change:likedPlaces', likedPlaces);
     },
 
